e99908797d7aa5bca7c179ced5d0df98c052f8c6
Rishabh <44425012+ioxvar@users.noreply.github.com>
2022-01-23 01:44:14 +0530

    random image from unsplash

--- randomizer/ViewController.swift
+++ randomizer/ViewController.swift
@@ -19,12 +19,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
+        view.backgroundColor = .systemBlue
         view.addSubview(imageView)
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         imageView.center = view.center
+        
+        getRandomisedPhoto()
     }
-
-
+    
+    func getRandomisedPhoto(){
+        let urlString = "https://source.unsplash.com/random/600x600"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else{
+            return
+        }
+        imageView.image = UIImage(data: data)
+    }
 }
 
